633df810eabdd8ef933b7653864ea5224b2333a2
ShashankaDhungana <shashanka.dd@gmail.com>
2022-01-20 22:24:05 +0545

    end of variable declaration

--- src/Specs/Specialist.java
+++ src/Specs/Specialist.java
@@ -175,6 +175,56 @@ public class Specialist extends javax.swing.JFrame {
     private void BackBtnActionPerformed(java.awt.event.ActionEvent evt) {
         // TODO add your handling code here:
     }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Specialist.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Specialist.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Specialist.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Specialist.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Specialist().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify
+    private javax.swing.JButton BackBtn;
+    private javax.swing.JLabel BackGround;
+    private javax.swing.JButton DermaBtn;
+    private javax.swing.JButton GynoBtn;
+    private javax.swing.JButton NeuroBtn;
+    private javax.swing.JButton OtpoBtn;
+    private javax.swing.JButton PedaBtn;
+    private javax.swing.JLabel SpecalistTxt;
+    private javax.swing.JPanel SpecialistBackground;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration
+}
+
 
 
 
